45762ce7a92e17718e271208bd798d4c5837b5a7
angad128 <ango1993a@gmail.com>
2019-05-31 00:16:15 +0545

    Root App component rendering three other components

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import MyNavbar from "./MyNavbar"
+import MyInfo from "./MyInfo"
+import Footer from "./Footer"
+
+function App() {
+	return(
+		<div>
+			<MyNavbar />
+			<MyInfo />
+			<Footer />
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/Footer.js
@@ -0,0 +1,93 @@
+import React from "react"
+import '../css/footer.css';
+
+function Footer() {
+	return (
+		<footer id="footer">
+			<div class="container-fluid" id="FAQs">
+			  <div class="container">
+			    <div class="row footer-dash">
+			        <div class="col-lg-5 col-sm-5 col-xs-12">
+			        <center>Image</center>
+			        <p>We believe in seamless, automatic service triumps last minute, on-demand requests and with our less than 30-seconds booking, secure payment, and 100% money-back guarantee, ThirdWheel aims to be the easiest and most convenient way to provide your vehicle the service it deserves. </p>
+			        <div class="row">
+			          <div class="col-md-6">
+			            <i class="fa fa-envelope" aria-hidden="true"><a href="mailto">&nbsp;support@thirdwheel.org</a></i>
+			          </div>
+			          <div class="col-md-6">
+			            <i class="fa fa-phone" aria-hidden="true">&nbsp;+977 9841522160</i>
+			          </div>
+			        </div>
+			      </div>
+			      <div class="col-md-7 col-sm-7 col-xs-12">
+			        <div class="row newsletter">
+			          <div class="col-md-4">
+			            <h4><b>Company</b></h4>
+			            <a href="#"><li>About Us</li></a>
+			            <a href="#"><li>Press</li></a>
+			            <a href="#"><li>Download App</li></a>
+			              <a href="#"><li>Terms and Condition</li></a>
+			          </div>
+			          <div class="col-md-4">
+			            <ul>
+			                 <li><h4><b>Policies</b></h4></li>      
+			                  <a href="#"><li>Terms and Condition</li></a>
+			              <a href="#"><li>Privacy Policy</li></a>
+			              <a href="#"><li>Refunds</li></a>
+			              <a href="#"><li>FAQs</li></a>
+			              </ul> 
+			          </div>
+			          <div class="col-md-4">  
+			              <ul>
+			                 <li><h4><b>Resources</b></h4></li>
+			                  <a href="#"><li>Blog</li></a>
+			                  <a href="#"><li>Ask Expert</li></a>
+			                  <a href="#"><li>Know your Mechanic</li></a>
+			                  <a href="#"><li>How it Works</li></a>
+			              </ul>
+			          </div>
+			        </div>
+			        <div class="row join-newsletter">
+			            <div class="col-md-12 col-lg-12 col-sm-12">
+			                <p><b>JOIN OUR NEWSLETTER</b></p>
+			                <div class="input-group input-group-lg">
+			                    <div class="input-type">
+			                        <input type="text"  aria-describedby="sizing-addon1" placeholder="Enter your email." />
+			                    </div>
+			                    <div class="input-link">
+			                        <a href="#" class="btn btn-primary btn-contact btn-large hvr-sweep-to-right"><center>Submit</center></a>
+			                    </div>
+			                </div>  
+			            </div>
+			        </div>     
+			      </div>
+			    </div>
+			  </div>
+			</div>
+			<div class="container-fluid footer">
+			  <div class="container">
+			    <div class="row">
+			        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+			        	<p>Copyright 2017. All Rights Reserved</p>
+			        </div>
+			        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+				        <center><h5>BE SOCIAL WITH US</h5></center>
+				        <center>
+				        	<a href="#" class="hvr-float-shadow"><i class="fa fa-facebook " aria-hidden="true"></i></a>
+				       		<a href="#" class="hvr-float-shadow"><i class="fa fa-google-plus " aria-hidden="true"></i></a>
+				        	<a href="#" class="hvr-float-shadow"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
+				        	<a href="#" class="hvr-float-shadow"><i class="fa fa-twitter " aria-hidden="true"></i></a>
+				        	<a href="#" class="hvr-float-shadow"><i class="fa fa-instagram " aria-hidden="true"></i></a>
+				      	</center>
+				    </div>
+			        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+			        	<p>Powered by><a href="#"><h6>Ango- ango1993@gamil.com</h6></a></p>
+			        </div>
+			    </div>
+			  </div>
+			</div>
+		</footer>
+	)
+}
+
+export default Footer
