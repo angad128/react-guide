3e4cb1fa0536a1eae9bd4cb6cf5540a676162c31
angad128 <ango1993a@gmail.com>
2019-06-05 23:22:00 +0545

    JSX and javascript variables. As per time of day, Greeting app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-function App(){
+function App() {
 	const date = new Date()
-
+	const hours = date.getHours()
+	let timeofDay
+	if(hours <12) {
+		timeofDay = "morning"
+	} else if(hours >=12 && hours <17) {
+		timeofDay = "afternoon"
+	} else {
+		timeofDay = "goodnight"
+	}
 	return (
-		<h1>It is currently about {date.getHours() % 12} o' clock!</h1>
+		<h1>Good {timeofDay}!</h1>
 	)
 }
 
-ReactDOM.render(<App />,document.getElementById("root"))
+
+ReactDOM.render(<App />, document.getElementById("root"))
