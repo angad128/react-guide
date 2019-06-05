fbcd376f269a3c726b947954fa6a2504b6ff64b3
angad128 <ango1993a@gmail.com>
2019-06-05 23:31:22 +0545

    JSX and javascript variables. As per time of day, Greeting app and inline styling in JSX

--- src/index.js
+++ src/index.js
@@ -5,15 +5,29 @@ function App() {
 	const date = new Date()
 	const hours = date.getHours()
 	let timeofDay
+
+	const myStyles = {
+		color: "#ff8c00", 
+		fontSize: "40px",
+		paddindTop: "20px",
+		paddindButtom: "20px",
+		paddindLeft: "20px",
+		paddindRight: "20px"
+	}
+
 	if(hours <12) {
 		timeofDay = "morning"
+		myStyles.backgroundColor = "green"
 	} else if(hours >=12 && hours <17) {
 		timeofDay = "afternoon"
+		myStyles.backgroundColor = "red"
 	} else {
 		timeofDay = "goodnight"
+		myStyles.backgroundColor = "yellow"
 	}
+
 	return (
-		<h1>Good {timeofDay}!</h1>
+		<h1 style={myStyles}>Good {timeofDay}!</h1>
 	)
 }
 
