d64c09ac9ef5e20fde2abd10c6254aff9e84d5dc
angad128 <ango1993a@gmail.com>
2019-06-29 22:44:24 +0545

    Props and its use to make dyanamic Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React from "react"
+import ContactCard from './ContactCard'
 
-import MyNavbar from "./MyNavbar"
-import MyInfo from "./MyInfo"
-import Footer from "./Footer"
 
-function App() {
-	return(
+
+function App(){
+	return (
 		<div>
-			<MyNavbar />
-			<MyInfo />
-			<Footer />
+			<ContactCard name="Mr.WHite" imgUrl="http://placekitten.com/300/200" phone="+977-9841 XXXXXX" email="mr.white@email.com"/>
+			<ContactCard name="Mr.red" imgUrl="http://placekitten.com/300/200" phone="+977-9843 XXXXXX" email="mr.red@email.com"/>
+			<ContactCard name="Mr.Black" imgUrl="http://placekitten.com/300/200" phone="+977-9842 XXXXXX" email="mr.black@email.com"/>
+			<ContactCard name="Mr.Yellow" imgUrl="http://placekitten.com/300/200" phone="+977-9845 XXXXXX" email="mr.yellow@email.com"/>
+			
 		</div>
 	)
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import '../css/todostyle.css';
+
+function TodoItem() {
+	return (
+		<div className="todo-Item">
+			<input type="checkbox" />
+			<p>Placeholder text here</p>
+		</div>
+	)
+}
+
+export default TodoItem
