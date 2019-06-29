8eb13c02fbf33287c001aab6fb4ad77e9ec9021e
angad128 <ango1993a@gmail.com>
2019-06-29 22:53:47 +0545

    Standart Concept for using and passing props/ helps when there is large numbers of props

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,10 @@ import ContactCard from './ContactCard'
 function App(){
 	return (
 		<div>
-			<ContactCard name="Mr.WHite" imgUrl="http://placekitten.com/300/200" phone="+977-9841 XXXXXX" email="mr.white@email.com"/>
-			<ContactCard name="Mr.red" imgUrl="http://placekitten.com/300/200" phone="+977-9843 XXXXXX" email="mr.red@email.com"/>
-			<ContactCard name="Mr.Black" imgUrl="http://placekitten.com/300/200" phone="+977-9842 XXXXXX" email="mr.black@email.com"/>
-			<ContactCard name="Mr.Yellow" imgUrl="http://placekitten.com/300/200" phone="+977-9845 XXXXXX" email="mr.yellow@email.com"/>
+			<ContactCard contact={{name:"Mr.WHite", imgUrl:"http://placekitten.com/300/200", phone:"+977-9841 XXXXXX", email:"mr.white@email.com" }}/>
+			<ContactCard contact={{name:"Mr.red", imgUrl:"http://placekitten.com/300/200", phone:"+977-9843 XXXXXX", email:"mr.red@email.com" }}/>
+			<ContactCard contact={{name:"Mr.Black", imgUrl:"http://placekitten.com/300/200", phone:"+977-9842 XXXXXX", email:"mr.black@email.com" }}/>
+			<ContactCard contact={{name:"Mr.Yellow", imgUrl:"http://placekitten.com/300/200", phone:"+977-9845 XXXXXX", email:"mr.yellow@email.com" }}/>
 			
 		</div>
 	)
